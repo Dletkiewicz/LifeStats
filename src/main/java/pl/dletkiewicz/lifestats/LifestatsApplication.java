d47017441441cebf4f43fdf8d1610a2f2aa2ecfe
dletkiewicz <jokerek99@gmail.com>
2026-02-21 18:54:42 +0100

    init

---
+++ src/main/java/pl/dletkiewicz/lifestats/LifestatsApplication.java
@@ -0,0 +1,13 @@
+package pl.dletkiewicz.lifestats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifestatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifestatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/dletkiewicz/lifestats/LifestatsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dletkiewicz.lifestats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifestatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
