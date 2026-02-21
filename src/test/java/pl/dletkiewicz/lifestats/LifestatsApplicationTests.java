dd01bd473c5d456faa44e23887960ae2bda565a1
dletkiewicz <jokerek99@gmail.com>
2026-02-21 19:44:23 +0100

    init docker, docker-compose, liquibase, application yaml

--- src/test/java/pl/dletkiewicz/lifestats/LifestatsApplicationTests.java
+++ src/test/java/pl/dletkiewicz/lifestats/LifestatsApplicationTests.java
@@ -3,10 +3,9 @@ package pl.dletkiewicz.lifestats;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class LifestatsApplicationTests {
 
-	@Test
+
 	void contextLoads() {
 	}
 
